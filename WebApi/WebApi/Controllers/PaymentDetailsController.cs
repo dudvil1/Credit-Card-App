ab91bcffb27d5d00b44af7796c719a5b607682f0
dudu <dudvil1@gmail.com>
2019-12-12 10:28:46 +0200

    final

---
+++ WebApi/WebApi/Controllers/PaymentDetailsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentDetailsController : ControllerBase
+    {
+        private readonly PaymentDetailsContext _context;
+
+        public PaymentDetailsController(PaymentDetailsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PaymentDetails
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PaymentDetails>>> GetpaymentDetails()
+       {
+            return await _context.paymentDetails.ToListAsync();
+        }
+
+        // GET: api/PaymentDetails/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PaymentDetails>> GetPaymentDetails(int id)
+        {
+            var paymentDetails = await _context.paymentDetails.FindAsync(id);
+
+            if (paymentDetails == null)
+            {
+                return NotFound();
+            }
+
+            return paymentDetails;
+        }
+
+        // PUT: api/PaymentDetails/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPaymentDetails(int id, PaymentDetails paymentDetails)
+        {
+            if (id != paymentDetails.PMId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(paymentDetails).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentDetailsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PaymentDetails
+        [HttpPost]
+        public async Task<ActionResult<PaymentDetails>> PostPaymentDetails([FromBody]PaymentDetails paymentDetails)
+        {
+            _context.paymentDetails.Add(paymentDetails);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPaymentDetails", new { id = paymentDetails.PMId }, paymentDetails);
+        }
+
+        // DELETE: api/PaymentDetails/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<PaymentDetails>> DeletePaymentDetails(int id)
+        {
+            var paymentDetails = await _context.paymentDetails.FindAsync(id);
+            if (paymentDetails == null)
+            {
+                return NotFound();
+            }
+
+            _context.paymentDetails.Remove(paymentDetails);
+            await _context.SaveChangesAsync();
+
+            return paymentDetails;
+        }
+
+        private bool PaymentDetailsExists(int id)
+        {
+            return _context.paymentDetails.Any(e => e.PMId == id);
+        }
+    }
+}
---
+++ WebApi/WebApi/Models/PaymentDetails.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class PaymentDetails
+    {
+        [Key]
+        public int PMId { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(16)")]
+        public int CardNumber { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExperationDate { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(3)")]
+        public int CVV { get; set; }
+    }
+}
